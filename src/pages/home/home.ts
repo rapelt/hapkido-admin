e1a8d0c48d76e3e68d85c59a5d32ae67b2db7f0d
Rebekah Apelt <rebekah_apelt@thoughtworks.com>
2017-07-20 16:24:21 +1000

    Tests for travis

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,9 +6,14 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
+  title: string = "Home";
 
   constructor(public navCtrl: NavController) {
 
+  }
+
+  changeTitle(title){
+    this.title = title;
   }
 
 }
